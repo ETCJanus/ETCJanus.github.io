43df868410fcfaee942f7691d99dc2b939b38502
ETCJanus <etcjanus@gmail.com>
2026-02-05 15:13:29 +0100

    Fix URL to root domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  // This turns on the styling engine
   integrations: [tailwind()],
-  
-  // Replace this with your actual GitHub URL later
-  site: 'https://yourusername.github.io',
+  site: 'https://etcjanus.github.io', // <--- Update this
+  // Remove any 'base' property if you added one
 });
